df7231bcca183223327dd51bfddbd575a932b13c
Pierito Rolando <rolandopiero63@gmail.com>
2020-12-26 17:17:02 -0500

    hago este commit antes de cagarme todo la pagina con alguna burrada

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,7 @@ class About extends React.Component{
         <div className="content">
             <div className="contentBx w50">
                 <h3>Soy un Full Stack Developer.</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+                <p>Hola amigo o amiga, como va tu dia, soy un Desarrollador Full Stack con 14 años, soy un apasionado de la tecnologia desde muy pequeño, aprendi a programar a los 13 y me actualizo de novedades y nunca paro de aprender.
                     <br/>
                     <br/>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
--- src/components/Services.js
+++ src/components/Services.js
@@ -10,37 +10,37 @@ render(){
             </div>
             <div className="content">
                 <div className="servicesBx">
-                    <img src='https://www.flaticon.com/svg/static/icons/svg/160/160138.svg' className="icon" alt="imagen"/>
+                    <img src='https://www.flaticon.com/svg/static/icons/svg/781/781683.svg' className="icon" alt="imagen"/>
                     <h2>Desarrollo Web</h2>
                     <p>Muy corta pero consisa experiencia maquetando y diseñando paginas webs, en mis proyectos para el area de desarrollo utilizo tecnologiales tales como ReactJS, NodeJS, express, Django y Firebase para salvarme del apuro, soy un amante del backend y del Frontend, pero mas del backend.</p>
                 </div>
 
                 <div className="servicesBx">
-                    <img src='https://www.flaticon.com/svg/static/icons/svg/160/160138.svg' className="icon" alt="imagen"/>
+                    <img src='https://www.flaticon.com/svg/static/icons/svg/3308/3308927.svg' className="icon" alt="imagen"/>
                     <h2>Edicion de Video</h2>
                     <p>Comenze a editar videos a mis 10 años de edad, podria decirce que tengo una mplia experiencia en el mundo de la produccion audiovisual, domino softwares tales como Adobe Phothoshop, Adobe Premiere, Adobe Aftter Effects y DaVinci Resolve, tambien tengo un canal de YouTube</p>
                 </div>
 
                 <div className="servicesBx">
-                    <img src='https://www.flaticon.com/svg/static/icons/svg/160/160138.svg' className="icon" alt="imagen"/>
+                    <img src='https://www.flaticon.es/svg/static/icons/svg/3942/3942067.svg' className="icon" alt="imagen"/>
                     <h2>Fotografo</h2>
                     <p>Para mi una camara es de los mejores inventos de la historia, en mis tiempos libres o viajes me gustaba documentar todo lo que estaba a mi alrededor, no tendria una gran camara, pero con un poco de ingenio y Adobe Lightroom, nada era imposible</p>
                 </div>
 
                 <div className="servicesBx">
-                    <img src='https://www.flaticon.com/svg/static/icons/svg/160/160138.svg' className="icon" alt="imagen"/>
+                    <img src='https://www.flaticon.es/svg/static/icons/svg/1576/1576613.svg' className="icon" alt="imagen"/>
                     <h2>Desarrollo de Apps Nativas</h2>
                     <p>Nunca me gusto el Responsive Web Desing, pero lo tenia que hacer, ¿Sabes por qué?, porque hoy en dia la mayoria de las personas prefieren hacer todo a travez de su dispositivo movil, todos los dias trato de mejorar mis habilidades en este campo, con tecnologias como React Native y Flutter.</p>
                 </div>
 
                 <div className="servicesBx">
-                    <img src='https://www.flaticon.com/svg/static/icons/svg/160/160138.svg' className="icon" alt="imagen"/>
+                    <img src='https://www.flaticon.es/svg/static/icons/svg/2166/2166796.svg' className="icon" alt="imagen"/>
                     <h2>Desarrollo backend</h2>
                     <p>Como mencioné anteriormente, me gusta mucho el desarrolo backend, quizas no seré el mejor (por ahora), pero por alguna razon las bases de datos, NodeJS, y las APIrest, me llaman mucho la atencion, trabajo todos los dias para ser mejor en este campo tan necesitado en la tecnologia</p>
                 </div>
 
                 <div className="servicesBx">
-                    <img src='https://www.flaticon.com/svg/static/icons/svg/160/160138.svg' className="icon" alt="imagen"/>
+                    <img src='https://www.flaticon.es/svg/static/icons/svg/33/33736.svg' className="icon" alt="imagen"/>
                     <h2>Futbolista</h2>
                     <p>Este es mas un hobby que quisiera que conozcan, sé que no viene mucho al caso, pero si por alguna razón necesitaras un arquero de futbol, aca lo tienes, jugue en varios equipos de no mucha categorias, pero si campeonatos competitivos, tales como tercera division y copa feredativa.</p>
                 </div>
